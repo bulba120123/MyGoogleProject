9e01a6d2229a02f977c3e0f78e2ba20f8a6e0b64
bulba120123 <bulba120123@gmail.com>
2024-08-26 10:38:27 +0900

    Feat: Add kubernetes Service

--- project-server/src/kubernetes/kubernetes.controller.ts
+++ project-server/src/kubernetes/kubernetes.controller.ts
@@ -1,7 +1,47 @@
-import { Controller } from '@nestjs/common';
+// src/kubernetes/kubernetes.controller.ts
+
+import { Controller, Delete, Param, Post, Body } from '@nestjs/common';
 import { KubernetesService } from './kubernetes.service';
 
 @Controller('kubernetes')
 export class KubernetesController {
   constructor(private readonly kubernetesService: KubernetesService) {}
+
+  @Post('namespace')
+  async createNamespace(@Body('name') name: string) {
+    const { response, body } =
+      await this.kubernetesService.createNamespace(name);
+    return { response, body };
+  }
+
+  @Post('pod')
+  async createPod(
+    @Body('namespace') namespace: string,
+    @Body('podSpec') podSpec: any,
+  ) {
+    const { response, body } = await this.kubernetesService.createPod(
+      namespace,
+      podSpec,
+    );
+    return { response, body };
+  }
+
+  @Delete('namespace/:name')
+  async deleteNamespace(@Param('name') name: string) {
+    const { response, body } =
+      await this.kubernetesService.deleteNamespace(name);
+    return { response, body };
+  }
+
+  @Delete('pod/:namespace/:podName')
+  async deletePod(
+    @Param('namespace') namespace: string,
+    @Param('podName') podName: string,
+  ) {
+    const { response, body } = await this.kubernetesService.deletePod(
+      namespace,
+      podName,
+    );
+    return { response, body };
+  }
 }
--- project-server/src/kubernetes/kubernetes.service.ts
+++ project-server/src/kubernetes/kubernetes.service.ts
@@ -1,4 +1,55 @@
+// src/kubernetes/kubernetes.service.ts
+
 import { Injectable } from '@nestjs/common';
+import * as k8s from '@kubernetes/client-node';
+import * as http from 'http';
 
 @Injectable()
-export class KubernetesService {}
+export class KubernetesService {
+  private k8sApi: k8s.CoreV1Api;
+
+  constructor() {
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
+    this.k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+  }
+
+  async createNamespace(namespace: string): Promise<{
+    response: http.IncomingMessage;
+    body: k8s.V1Namespace;
+  }> {
+    const ns = {
+      metadata: {
+        name: namespace,
+      },
+    };
+    return this.k8sApi.createNamespace(ns);
+  }
+
+  async createPod(
+    namespace: string,
+    podSpec: k8s.V1Pod,
+  ): Promise<{
+    response: http.IncomingMessage;
+    body: k8s.V1Pod;
+  }> {
+    return this.k8sApi.createNamespacedPod(namespace, podSpec);
+  }
+
+  async deleteNamespace(namespace: string): Promise<{
+    response: http.IncomingMessage;
+    body: k8s.V1Status;
+  }> {
+    return this.k8sApi.deleteNamespace(namespace);
+  }
+
+  async deletePod(
+    namespace: string,
+    podName: string,
+  ): Promise<{
+    response: http.IncomingMessage;
+    body: k8s.V1Pod;
+  }> {
+    return this.k8sApi.deleteNamespacedPod(podName, namespace);
+  }
+}
